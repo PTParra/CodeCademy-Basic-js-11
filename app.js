429bf9b8df1cfb189cb50b1b17f13bf8c7eb6851
PTParra <49121638+PTParra@users.noreply.github.com>
2024-10-16 16:14:42 +0200

    ejercicio resuelto

--- app.js
+++ app.js
@@ -4,3 +4,43 @@
     for Today’s Special. Use your knowledge of getters and setters to make sure anyone who uses the new function 
     can generate a meal and a price for Today’s Special without any embarrassing errors!
 */
+
+const menu = {
+    _meal: '',
+    _price: 0,
+    set meal(mealToCheck) {
+        if (typeof mealToCheck === 'string') {
+            this._meal = mealToCheck;
+        }
+    },
+    set price(priceToCheck) {
+        if (typeof priceToCheck === 'number') {
+            this._price = priceToCheck;
+        }
+    },
+    get todaysSpecial() {
+        if (this._meal && this._price) {
+            return "Today's Special is " + this._meal + " for $" + this._price;
+        } else {
+            return 'Meal or price was not set correctly!'
+        }
+    }
+};
+
+console.log(menu);
+
+console.log("Añadir lechuga al menu");
+
+menu.meal = 'lechuga';
+
+console.log(menu);
+
+console.log("Añadir precio al menu");
+
+menu.price = 10;
+
+console.log(menu);
+
+console.log("Devolver especial del dia");
+
+console.log(menu.todaysSpecial);
